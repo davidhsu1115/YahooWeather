2534debc58593208e9d9e0bfde396d53c3503b73
davidhsu1115 <davidhsu1115@gmail.com>
2015-08-04 14:29:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/davidhsu/yahooweather/YahooWeatherExceptionListener.java
@@ -0,0 +1,7 @@
+package com.example.davidhsu.yahooweather;
+
+public interface YahooWeatherExceptionListener {
+    public void onFailConnection(final Exception e);
+    public void onFailParsing(final Exception e);
+    public void onFailFindLocation(final Exception e);
+}
